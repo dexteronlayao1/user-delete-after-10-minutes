30508399c2e44ef507081e25adb4a17ee863d265
donlayao <dexter@netlinkvoice.com>
2022-02-09 09:53:38 +0800

    Dynamic Deletion of users

--- app/Console/Commands/DeleteUserCommand.php
+++ app/Console/Commands/DeleteUserCommand.php
@@ -39,6 +39,24 @@ class DeleteUserCommand extends Command
      */
     public function handle()
     {
-        DB::table('users')->whereRaw('TIMESTAMPDIFF(MINUTE, created_at, now()) > 10')->delete();
+        $timeFrame = $this->choice(
+            'Select the timeframe. If you did not select any of the timeframe, the default value would be Day',
+            ['Day', 'Hour', 'Minute', 'Second'],
+            0
+        );
+
+        $time = $this->ask("Input the number of $timeFrame/s");
+
+        $newTimeFrame = $time > 1 ? "{$timeFrame}s" : $timeFrame;
+
+        if ($this->confirm("Users that were created more than $time $newTimeFrame will be deleted. Do you wish to continue?")) {
+            try {
+                $usersCount = DB::table('users')->whereRaw("TIMESTAMPDIFF($timeFrame, created_at, now()) > $time")->count();
+                DB::table('users')->whereRaw("TIMESTAMPDIFF($timeFrame, created_at, now()) > $time")->delete();
+                $this->info("Number of users deleted: $usersCount");
+            } catch (\Exception $ex) {
+                $this->error('Something went wrong. Please double check the value you inserted');
+            }
+        }
     }
 }
